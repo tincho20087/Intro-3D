f188cf38ce7908bbd459b3a2c0ccf7c1e2e43257
tincho20087 <48802391@est.ort.edu.ar>
2025-05-15 12:00:17 -0300

    spawn cube

--- Assets/Scripts/ClonaCybos.cs
+++ Assets/Scripts/ClonaCybos.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class ClonaCybos : MonoBehaviour
 {
+    public GameObject cuboPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(cuboPrefab);
     }
 
     // Update is called once per frame
