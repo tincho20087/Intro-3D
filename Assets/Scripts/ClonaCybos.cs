2750a6c2814e5030974f30258d781f0f143868f6
tincho20087 <48802391@est.ort.edu.ar>
2025-05-15 12:09:04 -0300

    funciona boton clona cubos

--- Assets/Scripts/ClonaCybos.cs
+++ Assets/Scripts/ClonaCybos.cs
@@ -9,12 +9,17 @@ public class ClonaCybos : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(cuboPrefab);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ClonarCubo()
+    {
+        Instantiate(cuboPrefab);
     }
 }
